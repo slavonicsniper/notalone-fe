333c3ad16e0db9f4bbf8c6522bb00453a32a9412
Erik Galajda <e.galajda@comforte.com>
2022-04-06 19:12:27 +0200

    dashboard cards

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import User from '../../services/User';
+import {Row, Col, Card, Container, ListGroup} from 'react-bootstrap'
 
 export default function Dashboard() {
     const [users, setUsers] = useState([])
@@ -17,17 +18,29 @@ export default function Dashboard() {
     }, [])
 
     return (
-        <div>
-            {users.map(user => {
-                return (
-                    <div key={user.uuid}>
-                        <p>{user.username}</p>
-                        <p>{user.city}</p>
-                        <p>{user.country}</p>
-                        <p>{user.age}</p>
-                    </div>
-                )
-            })}
-        </div>
+        <Container>
+            <Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6} className="g-4">
+              {users.map((user, idx) => (
+                <Col>
+                  <Card key={idx}>
+                    <Card.Body>
+                        <Card.Title>{user.username}</Card.Title>
+                        <Card.Text>
+                          Hello, I am {user.username}. Nice to meet you!
+                        </Card.Text>
+                        <Card.Header>Activities</Card.Header>
+                        <Card.Text>
+                            {user.UserActivities.map(activity => activity.Activity.name).join(', ')}
+                        </Card.Text>
+                        <Card.Header>Availabilities</Card.Header>
+                        <Card.Text>
+                            {user.Availabilities.map(availability => `${availability.day} ${availability.start_time} - ${availability.end_time}`).join(', ')}
+                        </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+        </Container>
     )
 }
